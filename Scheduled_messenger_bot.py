ebfee0305270743a927894c143bc333a804e7bc9
Fitjko <68557462+Fitjko@users.noreply.github.com>
2022-01-18 19:18:02 +0100

    Update Scheduled_messenger_bot.py

--- Scheduled_messenger_bot.py
+++ Scheduled_messenger_bot.py
@@ -12,7 +12,7 @@ chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications" : 2}
 chrome_options.add_experimental_option("prefs",prefs)
 
-driver=webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.get('https://www.facebook.com/messages/t/')
 
 # email pasword and login elements stored in a variable
